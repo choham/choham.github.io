08d0bc5622072c09c06750750a37ac0cc1d33451
StevenChu <choham1023@gmail.com>
2023-05-16 15:13:34 +0800

    Add to get USB device

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-document.getElementById('search HID device').addEventListener('click', showAllDevice);
+document.getElementById('search HID device').addEventListener('click', showAllHIDDevice);
+document.getElementById('search USB device').addEventListener('click', showAllUSBDevice);
 
-async function showAllDevice() {
+async function showAllHIDDevice() {
     const device_list = await navigator.hid.getDevices();
+    console.log(device_list)
+}
+
+async function showAllUSBice() {
+    const device_list = await navigator.usbtDevices();
     console.log(device_list)
 }
